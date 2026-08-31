a1c3527100e9a96f7777759e8755997d4771ac50
Gagaaa21 <marcosimonovich27@gmail.com>
2026-08-31 21:15:36 +0200

    Delete src directory

--- src/lib/api/forecast.functions.ts
+++
@@ -1,8 +0,0 @@
-import { createServerFn } from "@tanstack/react-start";
-import { requireSupabaseAuth } from "@/integrations/supabase/auth-middleware";
-import { getForecastData } from "./forecast.server";
-import type { ForecastResponse } from "./forecast.types";
-
-export const getForecast = createServerFn({ method: "POST" })
-  .middleware([requireSupabaseAuth])
-  .handler(async (): Promise<ForecastResponse> => getForecastData());
